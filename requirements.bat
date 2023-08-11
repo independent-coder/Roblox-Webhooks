b307ab925bf4ec1894bba555a211a24a16c2d301
Mr.Programs <127637860+independent-coder@users.noreply.github.com>
2023-08-11 18:54:59 -0400

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,2 +1,15 @@
 @echo off
-pip install robloxpy requests browser_cookie3
+
+rem Check if Python is installed
+python --version > nul 2>&1
+
+if %errorlevel% equ 0 (
+    rem Python is installed, continue with pip installation
+    pip install robloxpy requests browser_cookie3 discord psutil pyautogui discord_webhook
+) else (
+    rem Python is not found, display an error message
+    echo Python was not found. Please install the latest version of Python.
+)
+
+pause
+exit
